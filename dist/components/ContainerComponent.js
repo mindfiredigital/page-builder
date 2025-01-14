2aff1ed961a9b65e7c377b4d334b4aea3ffa5dd5
SubhenduS1999 <subhendus@mindfiresolutions.com>
2025-01-14 14:51:03 +0530

    fix(container dragable issue): container drag always shifting to the left side

--- dist/components/ContainerComponent.js
+++ dist/components/ContainerComponent.js
@@ -114,9 +114,9 @@ export class ContainerComponent {
     window.addEventListener('mouseup', this.stopResize);
   }
   initializeEventListeners() {
-    this.element.addEventListener('dragover', event => event.preventDefault());
     this.element.addEventListener('dragstart', this.onDragStart.bind(this));
     this.element.addEventListener('drop', this.onDrop.bind(this));
+    this.element.addEventListener('dragover', event => event.preventDefault());
     this.element.addEventListener('mouseover', this.onMouseOver.bind(this));
     this.element.addEventListener('mouseleave', this.onMouseLeave.bind(this));
   }
@@ -226,7 +226,7 @@ export class ContainerComponent {
     const style = document.createElement('style');
     style.textContent = `
       .container-component {
-        position: relative;
+        position: relative !important;
         display: flex;
         min-width: 100px;
         min-height: 100px;
