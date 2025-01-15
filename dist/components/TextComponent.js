dbd13d7494961d23fd1e329396df091c2098129c
Ayusman Parida <ayusmanp@mindfiresolutions.com>
2025-01-15 12:55:53 +0530

    fix: fix appearance of extra delete button on column
    Fix appearance of extra delete button during text component as child
    
    Ref: []

--- dist/components/TextComponent.js
+++ dist/components/TextComponent.js
@@ -3,7 +3,7 @@ export class TextComponent {
     this.text = text; // Default text value
   }
   create() {
-    const element = document.createElement('p');
+    const element = document.createElement('div');
     element.innerText = this.text; // Use dynamic text passed in constructor
     element.contentEditable = 'true'; // Enable inline editing
     element.classList.add('text-component');
