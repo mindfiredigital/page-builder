4672641bc2a3cc5c661e8d4724ef08fcc13d57d6
anandmindfire <anand.k@mindfiresolutions.com>
2025-01-16 15:41:01 +0530

    fix: fix resizer in header

--- dist/components/HeaderComponent.d.ts
+++ dist/components/HeaderComponent.d.ts
@@ -1,20 +1,3 @@
 export declare class HeaderComponent {
-  private element;
-  private resizers;
-  private readonly MINIMUM_SIZE;
-  private originalWidth;
-  private originalHeight;
-  private originalX;
-  private originalY;
-  private originalMouseX;
-  private originalMouseY;
-  private currentResizer;
-  constructor(level?: number, text?: string);
-  private addResizeHandles;
-  private initResize;
-  private resize;
-  private stopResize;
-  private addStyles;
-  private initializeEventListeners;
-  create(): HTMLElement;
+  create(level?: number, text?: string): HTMLElement;
 }
