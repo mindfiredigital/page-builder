8d39e5c2c4b1d94f19c7cfdd1fbdd0e284480a91
Ayusman Parida <ayusmanp@mindfiresolutions.com>
2025-02-20 11:58:44 +0530

    fix(react): remove the core page builder package
    Remove the code page builder package
    
    Refs:[]

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -5,10 +5,6 @@ var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __esm = (fn, res) =>
-  function __init() {
-    return fn && (res = (0, fn[__getOwnPropNames(fn)[0]])((fn = 0))), res;
-  };
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
@@ -40,67 +36,6 @@ var __toESM = (mod, isNodeMode, target) => (
 var __toCommonJS = mod =>
   __copyProps(__defProp({}, '__esModule', { value: true }), mod);
 
-// ../../node_modules/.pnpm/@mindfiredigital+page-builder-web-component@1.1.1_@mindfiredigital+page-builder-core@packages+core/node_modules/@mindfiredigital/page-builder-web-component/dist/index.js
-var dist_exports = {};
-__export(dist_exports, {
-  PageBuilderComponent: () => PageBuilderComponent,
-});
-var import_PageBuilder, PageBuilderComponent;
-var init_dist = __esm({
-  '../../node_modules/.pnpm/@mindfiredigital+page-builder-web-component@1.1.1_@mindfiredigital+page-builder-core@packages+core/node_modules/@mindfiredigital/page-builder-web-component/dist/index.js'() {
-    import_PageBuilder = require('@mindfiredigital/page-builder-core/dist/PageBuilder.js');
-    PageBuilderComponent = class extends HTMLElement {
-      constructor() {
-        super();
-        this.initialized = false;
-        this.template = `<div id="app">
-      <div id="sidebar"></div>
-      <div id="canvas" class="canvas"></div>
-      <div id="customization">
-        <h4 id="component-name">Component: None</h4>
-        <div id="controls"></div>
-        <div id="layers-view" class="hidden"></div>
-      </div>
-      <div id="notification" class="notification hidden"></div>
-      <div id="dialog" class="dialog hidden">
-        <div class="dialog-content">
-          <p id="dialog-message"></p>
-          <button id="dialog-yes" class="dialog-btn">Yes</button>
-          <button id="dialog-no" class="dialog-btn">No</button>
-        </div>
-      </div>
-    </div>`;
-        if (!this.firstElementChild) {
-          this.innerHTML = this.template;
-        }
-      }
-      // Lifecycle method: Called when the element is added to the DOM
-      connectedCallback() {
-        if (this.initialized) {
-          return;
-        }
-        this.initializePageBuilder();
-      }
-      // Initializes the PageBuilder instance
-      initializePageBuilder() {
-        if (this.initialized) {
-          return;
-        }
-        try {
-          this.initialized = true;
-          this.pageBuilder = new import_PageBuilder.PageBuilder();
-        } catch (error) {
-          console.error('Failed to initialize PageBuilder:', error);
-          this.initialized = false;
-        }
-      }
-    };
-    if (!customElements.get('page-builder')) {
-      customElements.define('page-builder', PageBuilderComponent);
-    }
-  },
-});
-
 // src/index.tsx
 var src_exports = {};
 __export(src_exports, {
@@ -112,7 +47,7 @@ module.exports = __toCommonJS(src_exports);
 var import_react = __toESM(require('react'));
 var PageBuilderReact = () => {
   (0, import_react.useEffect)(() => {
-    Promise.resolve().then(() => init_dist());
+    import('@mindfiredigital/page-builder-web-component');
   }, []);
   return /* @__PURE__ */ import_react.default.createElement('page-builder', {
     style: { width: '100vw', height: '100vh' },
--- packages/react/dist/index.mjs
+++ packages/react/dist/index.mjs
@@ -1,79 +1,8 @@
-var __defProp = Object.defineProperty;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __esm = (fn, res) => function __init() {
-  return fn && (res = (0, fn[__getOwnPropNames(fn)[0]])(fn = 0)), res;
-};
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-
-// ../../node_modules/.pnpm/@mindfiredigital+page-builder-web-component@1.1.1_@mindfiredigital+page-builder-core@packages+core/node_modules/@mindfiredigital/page-builder-web-component/dist/index.js
-var dist_exports = {};
-__export(dist_exports, {
-  PageBuilderComponent: () => PageBuilderComponent
-});
-import { PageBuilder } from "@mindfiredigital/page-builder-core/dist/PageBuilder.js";
-var PageBuilderComponent;
-var init_dist = __esm({
-  "../../node_modules/.pnpm/@mindfiredigital+page-builder-web-component@1.1.1_@mindfiredigital+page-builder-core@packages+core/node_modules/@mindfiredigital/page-builder-web-component/dist/index.js"() {
-    PageBuilderComponent = class extends HTMLElement {
-      constructor() {
-        super();
-        this.initialized = false;
-        this.template = `<div id="app">
-      <div id="sidebar"></div>
-      <div id="canvas" class="canvas"></div>
-      <div id="customization">
-        <h4 id="component-name">Component: None</h4>
-        <div id="controls"></div>
-        <div id="layers-view" class="hidden"></div>
-      </div>
-      <div id="notification" class="notification hidden"></div>
-      <div id="dialog" class="dialog hidden">
-        <div class="dialog-content">
-          <p id="dialog-message"></p>
-          <button id="dialog-yes" class="dialog-btn">Yes</button>
-          <button id="dialog-no" class="dialog-btn">No</button>
-        </div>
-      </div>
-    </div>`;
-        if (!this.firstElementChild) {
-          this.innerHTML = this.template;
-        }
-      }
-      // Lifecycle method: Called when the element is added to the DOM
-      connectedCallback() {
-        if (this.initialized) {
-          return;
-        }
-        this.initializePageBuilder();
-      }
-      // Initializes the PageBuilder instance
-      initializePageBuilder() {
-        if (this.initialized) {
-          return;
-        }
-        try {
-          this.initialized = true;
-          this.pageBuilder = new PageBuilder();
-        } catch (error) {
-          console.error("Failed to initialize PageBuilder:", error);
-          this.initialized = false;
-        }
-      }
-    };
-    if (!customElements.get("page-builder")) {
-      customElements.define("page-builder", PageBuilderComponent);
-    }
-  }
-});
-
 // src/components/PageBuilder.tsx
 import React, { useEffect } from "react";
 var PageBuilderReact = () => {
   useEffect(() => {
-    Promise.resolve().then(() => init_dist());
+    import("@mindfiredigital/page-builder-web-component");
   }, []);
   return /* @__PURE__ */ React.createElement("page-builder", { style: { width: "100vw", height: "100vh" } });
 };
